6d77cea1c4a3b1ba0a339df854820fbee1d30434
Johann J Cardenas <johannc2@illinois.edu>
2024-02-07 13:46:47 -0600

    Update user authentication logic

--- Scientific Visualization/3DPM-Plot_TimeHistory.py
+++ Scientific Visualization/3DPM-Plot_TimeHistory.py
@@ -25,146 +25,57 @@ import os
 ########            User Inputs           ######## 
 ##################################################
 
-CaseList = ['CC71DS_P5_AC1S_B1_SB1_SG1']
-tstep = [9, 16]                          # Range of time steps to be analyzed
+CaseList = {'CC71DS_P5_AC1S_B1_SB1_SG1': list(range(1, 16)),
+            'CC72DS_P4_AC1S_B1_SB1_SG1': list(range(1, 29)),
+            'CC73DS_P5_AC1S_B1_SB1_SG1': list(range(1, 40)),}
+
+# Loading Nomenclature
+# P1 = 34.5 kip
+# P2 = 35.5 kip
+# P3 = 37.0 kip
+# P4 = 38.0 kip
+# P5 = 39.0 kip
+# P6 = 40.5 kip
+# P7 = 45.0 kip
+# P8 = 46.0 kip
+# P9 = 55.0 kip
+# P10 = 65.0 kip
 
 # Model Dimensions (Update per case)
-L = 32677.0        # Length of the Model
-Xw = 1677.0        # Length of the Wheel Path
-B = 32750.0        # Width of the Model
-Depth = 15000.0    # Total Depth[mm] of the Model
+L =  [32677.0, 34120.0,  35507.0]        # Length of the Model
+Xw = [1677.0, 3120.0, 4507.0]            # Length of the Wheel Path
+B =  [32750.0, 32750.0,  32750.0]        # Width of the Model
+b = [1750.0, 1750.0, 1750.0]             # Width of the Wheel Path
+Depth = [15000.0,  15000.0,  15000.0]    # Total Depth[mm] of the Model
 
 Structure = ['AC1', 'B1', 'SB1', 'SG1']   # Pavement Layers
 MyLabels = ['P-401', 'P-209', 'P-154', 'Subgrade'] # Plot Labels
 Thicks = [75.0, 150.0, 500.0, 14275.0]    # Thickness of each layer
 
-user = 'johannc2'
-directory = f'C:/Users/{user}/Box/FAA Data Project/04_FEM/00_FEM DATA/FAA_South/FAA_South_Responses/{CaseList[0]}/'
+user = 'johan'
+directory = f'C:/Users/{user}/Box/FAA Data Project/04_FEM/00_FEM DATA/FAA_South/FAA_South_Responses/'
 
-# Layer of Analysis
-la = 'AC1'
 
 ##################################################
 ########     Preliminary Calculations     ######## 
 ##################################################
 
-myData = []
-myData_all = []
-myE11max, myE22max, myE23max, myE33max = [], [], [], []
-myE11min, myE22min, myE23min, myE33min = [], [], [], []
-myS11max, myS22max, myS23max, myS33max = [], [], [], []
-myS11min, myS22min, myS23min, myS33min = [], [], [], []
+myData_layers = []  # Outer list to contain data for all cases
+myData_cases = []   # Outer list to store case-specific data
+
+for case in CaseList.keys():
+    case_layers = {layer: [] for layer in Structure}  # A dictionary for each case, keys being layers
+    myData_layers.append(case_layers)
+    myData_cases.append([])  # Still keeping it for storing case-specific data without layer distinction
 
 y_ranges = {}
 cumulative_thickness = 0
 for layer, thickness in zip(Structure, Thicks):
-    y_ranges[layer] = (Depth - cumulative_thickness - thickness, Depth - cumulative_thickness)
+    y_ranges[layer] = (Depth[0] - cumulative_thickness - thickness, Depth[0] - cumulative_thickness)
     cumulative_thickness += thickness
 
-for c in CaseList:
-    for ts in range(tstep[0], tstep[-1]):
-        filename = f'{c}_3DResponse_tire{ts}.txt'
-        filepath = os.path.join(directory, filename)
-        
-        # Remove the header
-        df = pd.read_csv(filepath, sep='\t')
-        data_all = df.copy()
-        data_all = data_all.sort_values(by='Node', ascending=True)
-        
-        y_lower, y_upper = y_ranges[la]
-        
-        # Sort the dataframe by 'Node'. Lowes node numbers first.
-        df = df.sort_values(by='Node', ascending=True)
-        # Filter dataframe based on the Yn coordinate
-        df_layer = df[df['Yn_elem'].between(y_lower, y_upper)]
-        
-        if la == Structure[0]:  # First layer (top)
-            df_new = df_layer.drop_duplicates(subset=['Xn_elem', 'Yn_elem', 'Zn_elem'], keep='first')
-            
-        elif la == Structure[-1]:  # Last layer (bottom)
-            df_new = df_layer.drop_duplicates(subset=['Xn_elem', 'Yn_elem', 'Zn_elem'], keep='last')
-        
-        else:  # Intermediate layers
-    
-            df_int_high = df_layer[df_layer['Yn_elem'] == y_upper]
-            df_int_high = df_int_high.drop_duplicates(subset=['Xn_elem', 'Yn_elem', 'Zn_elem'], keep='last')
-        
-            df_int_low = df_layer[df_layer['Yn_elem'] == y_lower]
-            df_int_low = df_int_low.drop_duplicates(subset=['Xn_elem', 'Yn_elem', 'Zn_elem'], keep='first')
-        
-            df_new = pd.concat([
-                df_layer[(df_layer['Yn_elem'] > y_lower) & (df_layer['Yn_elem'] < y_upper)],
-                df_int_low,
-                df_int_high
-            ])        
-        
-        dataly = df_new.copy()
-        
-        ##################################################
-        ########        STRAIN EXTRACTION         ######## 
-        ##################################################
-        
-        myE11max.append(dataly['E11'].max())
-        myE11min.append(dataly['E11'].min())
-        
-        myE22max.append(dataly['E22'].max())
-        myE22min.append(dataly['E22'].min())
-        
-        myE23max.append(dataly['E23'].max())
-        myE23min.append(dataly['E23'].min())
-        
-        myE33max.append(dataly['E33'].max())
-        myE33min.append(dataly['E33'].min())
-    
-        ##################################################
-        ########        STRESS EXTRACTION         ######## 
-        ##################################################
-                
-        myS11max.append(dataly['S11'].max())
-        myS11min.append(dataly['S11'].min())
-        
-        myS22max.append(dataly['S22'].max())
-        myS22min.append(dataly['S22'].min())
-        
-        myS23max.append(dataly['S23'].max())
-        myS23min.append(dataly['S23'].min())
-        
-        myS33max.append(dataly['S33'].max())
-        myS33min.append(dataly['S33'].min())
-
-        myData_all.append(data_all)
-        myData.append(dataly)
-
-
-# Get the index of the maximum value within E11max
-max_E11_idx = myE11max.index(max(myE11max))
-min_E11_idx = myE11min.index(min(myE11min))
-
-max_E22_idx = myE22max.index(max(myE22max))
-min_E22_idx = myE22min.index(min(myE22min))
-
-max_E23_idx = myE23max.index(max(myE23max))
-min_E23_idx = myE23min.index(min(myE23min))
-
-max_E33_idx = myE33max.index(max(myE33max))
-min_E33_idx = myE33min.index(min(myE33min))
-
-
-max_S11_idx = myS11max.index(max(myS11max))
-min_S11_idx = myS11min.index(min(myS11min))
-
-max_S22_idx = myS22max.index(max(myS22max))
-min_S22_idx = myS22min.index(min(myS22min))
-
-max_S23_idx = myS23max.index(max(myS23max))
-min_S23_idx = myS23min.index(min(myS23min))
-
-max_S33_idx = myS33max.index(max(myS33max))
-min_S33_idx = myS33min.index(min(myS33min))
-
-
 ##################################################
-########       LAYER SEGMENTATION         ######## 
+########     SEGMENTATION FUNCTION        ######## 
 ##################################################
 
 def process_layers(dataf):
@@ -188,92 +99,187 @@ def process_layers(dataf):
             dataframes[i] = pd.concat([dfl[(dfl['Yn_elem'] > y_lower) & (dfl['Yn_elem'] < y_upper)], df_int_low, df_int_high])
     
     return dataframes
+
+##################################################
+
+k = 0
+for case, values in CaseList.items():   
+    for ts in values:
+        filename = f'{case}_3DResponse_tire{ts}.txt'
+        filepath = os.path.join(directory + case + '/', filename)
         
+        # Reading the file for each tire state
+        df = pd.read_csv(filepath, sep='\t')
+        data_case = df.copy()
+        data_case = data_case.sort_values(by='Node', ascending=True)
+        
+        # Process data for each layer
+        data_layers = process_layers(data_case)  # Utilize the existing 'process_layers' function
+        
+        for layer_name, data_layer in zip(Structure, data_layers):
+            myData_layers[k][layer_name].append(data_layer)  # Append processed layer data to the corresponding case and layer
+        
+        myData_cases[k].append(data_case)  # Append the unsegmented case data as before
+    
+    k += 1
+    
+#________________________________________________________________________________________
+# Break to start plotting function
+#%% 
+
 ##################################################
 ########           STRAIN PLOTS           ######## 
 ##################################################
 
+plt.rcParams.update({'font.size': 12})
+
+
+locations = []
+labels = ['Single, 39 kips', 'Tandem, 38 kips', 'Tridem, 39 kips']
+markers = ['o', 's', 'D']
+colors = ['r', 'b', 'g']
+
+selected_ly = 'AC1'
+loc_rel = [1443.0, 50.0 , 730.0 ]
+
 # Longitudinal Profile Along the Z Axis
-def plot_E_1DY(strain_component, myData):
-    
-    if strain_component == 'E11':
-        data = myData[max_E11_idx].copy()
-        label = 'Longitudinal Strain'
-        idx = max_E11_idx
-        max_strain_value = data[strain_component].max()
-        location = data.loc[data[strain_component].idxmax(), ['Xn_elem', 'Yn_elem', 'Zn_elem']]
-        #location
-        strain_filtered = [myData[i].loc[(myData[i]['Xn_elem'] == location['Xn_elem']) & (myData[i]['Yn_elem'] == location['Yn_elem']) & (myData[i]['Zn_elem'] == location['Zn_elem']), strain_component] for i in range(len(myData))]
-        strain_values = [series.iloc[0]*1000000 for series in strain_filtered]
+def plot_ETH(field, myData):
+    strain_values = []
+    for _ in CaseList.keys():
+        strain_values.append([])
          
-    if strain_component == 'E33':
-        data = myData[max_E33_idx].copy()
-        label = 'Transverse Strain'
-        idx = max_E33_idx
-        max_strain_value = data[strain_component].max()
-        location = data.loc[data[strain_component].idxmax(), ['Xn_elem', 'Yn_elem', 'Zn_elem']]
-        strain_filtered = [myData[i].loc[(myData[i]['Xn_elem'] == location['Xn_elem']) & (myData[i]['Yn_elem'] == location['Yn_elem']) & (myData[i]['Zn_elem'] == location['Zn_elem']), strain_component] for i in range(len(myData))]
-        strain_values = [series.iloc[0]*1000000 for series in strain_filtered]
+    for case_idx, (case, values) in enumerate(CaseList.items()): 
+        location = [(L[case_idx]/2 - Xw[case_idx]/2) + loc_rel[0], 
+                    Depth[case_idx] - loc_rel[1], 
+                    B[case_idx]/2 - loc_rel[2]]
+        locations.append(location)
         
-    if strain_component == 'E22':
-        data = myData[min_E22_idx].copy()
-        label = 'Vertical Strain'
-        idx = min_E22_idx
-        max_strain_value = data[strain_component].min()
-        location = data.loc[data[strain_component].idxmin(), ['Xn_elem', 'Yn_elem', 'Zn_elem']]
-        strain_filtered = [myData[i].loc[(myData[i]['Xn_elem'] == location['Xn_elem']) & (myData[i]['Yn_elem'] == location['Yn_elem']) & (myData[i]['Zn_elem'] == location['Zn_elem']), strain_component] for i in range(len(myData))]
-        strain_values = [series.iloc[0]*1000000 for series in strain_filtered]
+        for ts in values:
+            data_ts = myData[case_idx][selected_ly][ts-1].copy()  # Access 'AC1' layer data for this tire state
+            
+            distances = np.sqrt((data_ts['Xn_elem'] - location[0]) ** 2 + 
+                                (data_ts['Yn_elem'] - location[1]) ** 2 + 
+                                (data_ts['Zn_elem'] - location[2]) ** 2)
+            idx_min = distances.idxmin()  # Index of the closest point
+            data_ts = data_ts.loc[[idx_min]]
 
-    if strain_component == 'E23':
-        data = myData[min_E23_idx].copy()
-        label = 'Shear Strain'
-        
-        if abs(data[strain_component].max()) >= abs(data[strain_component].min()):
-            idx = max_E23_idx
-            max_strain_value = data[strain_component].max()
-            location = data.loc[data[strain_component].idxmax(), ['Xn_elem', 'Yn_elem', 'Zn_elem']]
-            strain_filtered = [myData[i].loc[(myData[i]['Xn_elem'] == location['Xn_elem']) & (myData[i]['Yn_elem'] == location['Yn_elem']) & (myData[i]['Zn_elem'] == location['Zn_elem']), strain_component] for i in range(len(myData))]
-            strain_values = [series.iloc[0]*1000000 for series in strain_filtered]
-        else:
-            idx = min_E23_idx
-            max_strain_value = data[strain_component].min()
-            location = data.loc[data[strain_component].idxmin(), ['Xn_elem', 'Yn_elem', 'Zn_elem']]
-            strain_filtered = [myData[i].loc[(myData[i]['Xn_elem'] == location['Xn_elem']) & (myData[i]['Yn_elem'] == location['Yn_elem']) & (myData[i]['Zn_elem'] == location['Zn_elem']), strain_component] for i in range(len(myData))]
-            strain_values = [series.iloc[0]*1000000 for series in strain_filtered]
-        
-    time_labels = [i for i in range(tstep[0], tstep[-1])]
+            if field == 'U2':
+                label = r'Deflection $U_2$ (mm)'
+                strain_value = data_ts[field].iloc[0]
 
-    # Create the line plot
-    plt.figure(figsize=(8, 5))
-    plt.plot(strain_values, color='b', marker='o', linewidth=2.0, markersize=6.0)
-    plt.grid(True, linestyle='--', color='0.8', linewidth=0.5)
-    
-    plt.xticks(np.arange(0, len(time_labels), 1), time_labels)
+            elif field == 'E11':
+                label = r"Longitudinal Tensile Strain $E_{11}$ ($\mu\epsilon$)"
+                strain_value = data_ts[field].iloc[0]*1000000
+                
+            elif field == 'E22':
+                label = r"Vertical Compressive Strain $E_{22}$ ($\mu\epsilon$)"
+                strain_value = data_ts[field].iloc[0]*1000000
+                
+            elif field == 'E23':
+                label = r"Vertical Shear Strain $E_{23}$ ($\mu\epsilon$)"
+                strain_value = data_ts[field].iloc[0]*1000000
+            
+            elif field == 'E33':
+                label = r"Transverse Tensile Strain $E_{33}$ ($\mu\epsilon$)"
+                strain_value = data_ts[field].iloc[0]*1000000
+                
+            strain_values[case_idx].append(strain_value)
+            
         
-    plt.ylabel(f'{label}'+ ' ' + f'{strain_component} ($\mu\epsilon$)', fontweight='bold', fontsize=12)
-    plt.xlabel('Time Step', fontweight='bold', fontsize=12)
+    fig, ax = plt.subplots(figsize=(6,5), dpi=300)
+    for i, case in enumerate(CaseList.keys()):
+        ax.plot(strain_values[i], color=colors[i], marker=markers[i], 
+                markersize=5, linestyle='-',linewidth=2, label=labels[i])
     
-    # Plot point at location of the maximum strain value
-    plt.plot(idx, max_strain_value*1000000, color='r', 
-             marker='o', markersize=6.0, markeredgecolor='black', markeredgewidth=1.0)
+    ax.set_ylabel(label, fontweight='bold', fontsize=14)
+    ax.set_xlabel('Time Step', fontweight='bold', fontsize=14)
+    ax.set_xlim(0, len(CaseList[case]))
+    ax.legend()
     
-    # Textbox
-    # textstr = (f"${strain_component}_{{max}}$: {max_strain_value*1000000:.1f} $\mu\epsilon$")
-    # # Position the text box in figure coords to ensure it's always in the same position regardless of data
-    # props = dict(boxstyle="round4,pad=0.5", edgecolor='black', facecolor='white', linewidth=2)
-    # plt.text(idx, max_strain_value*1000000 + 25, textstr, fontsize=12, verticalalignment='bottom', horizontalalignment='center', bbox=props)
-    
-    plt.grid(True)
-    
-    plt.savefig(f'{la}'+ '_' +f'{strain_component}_Time_History.png', dpi=500, bbox_inches='tight')
+    plt.savefig(f'{selected_ly}_{field}_TimeHistory.png', dpi=300, bbox_inches='tight')
+
     plt.show()
+    plt.close()
     
-      
+
+    
+def plot_EMax(fields, myData):
+    # Assuming 'selected_ly' is defined
+    selected_ly = 'AC1'
+    label_selected_ly = 'Asphalt Layer'
+    # Assuming 'colors' is defined with enough colors for your fields
+    colors = ['r', 'g', 'b', 'y']  # Example: Different color for each field
+    labels = [r"$E_{22}$",
+              r"$E_{11}$",
+              r"$E_{33}$",
+              r"$E_{23}$"]
+
+    legend_labels = ['Single, 39 kips', 'Tandem, 38 kips', 'Tridem, 39 kips']
+    
+    fig, ax = plt.subplots(figsize=(6, 4), dpi=300)
+    width = 0.30  # Bar width
+    num_cases = 3  # Number of cases per field
+    num_fields = len(fields)
+
+    for field_idx, field in enumerate(fields):
+        
+        for case_idx, (case, values) in enumerate(CaseList.items()):
+            max_strains = []
+
+            for ts in values:
+                data_ts = myData[case_idx][selected_ly][ts-1].copy()
+                # Initialize strain_value at a safe default, if necessary
+                strain_value = 0  # Default value, in case the field does not match
+                
+                if field == 'U2':
+                    label = r'Deflection $U_2$ (mm)'
+                    strain_value = data_ts[field].max()
+                elif field in ['E22', 'E11', 'E33', 'E23']:
+                    if field == 'E11':
+                        strain_value = abs(data_ts[field].max()) * 1000000  # Absolute value of the maximum
+                    elif field == 'E22':
+                        strain_value = abs(data_ts[field].min()) * 1000000  # Absolute value of the minimum
+                    elif field == 'E33':
+                        strain_value = abs(data_ts[field].max()) * 1000000  # Absolute value of the maximum
+                    elif field == 'E23':
+                        max_value = abs(data_ts[field].max()) * 1000000  # Absolute value of the maximum
+                        min_value = abs(data_ts[field].min()) * 1000000  # Absolute value of the minimum
+                        strain_value = max(max_value, min_value)  # Maximum between the absolute values of maximum and minimum
+                
+                max_strains.append(strain_value)
+            
+            # Calculate the position for each bar to create a group effect
+            positions = [x + (case_idx - (num_cases / 2)) * width for x in range(num_fields)]
+            # Plot
+            ax.bar(positions[field_idx], max(max_strains), width, label=f'{case}', color=colors[case_idx])
+
+    ax.set_ylabel(r"Maximum Strain ($\mu\epsilon$)", fontweight='bold', fontsize=14)
+    ax.set_xlabel("Strain Fields", fontweight='bold', fontsize=14)
+    ax.set_xticks([i-width/2 for i in range(num_fields)])  # Center x-ticks between groups
+    ax.set_xticklabels(labels, fontsize=14)  # Use custom labels
+    ax.legend(fontsize=12, labels=legend_labels)
+    ax.title.set_text(f'{label_selected_ly}')
+    ax.title.set_fontsize(14)
+    ax.title.set_fontweight('bold')
+    
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[:num_cases], legend_labels[:num_cases], fontsize=10)
+
+    plt.savefig(f'{selected_ly}_MaxStrains.png', dpi=300, bbox_inches='tight')
+    plt.show()
+    plt.close()
+
+
 ##################################################
 ########             PLOTS                ######## 
 ##################################################
 
-plot_E_1DY('E22', myData)
-plot_E_1DY('E11', myData)
-plot_E_1DY('E33', myData)
-plot_E_1DY('E23', myData)
+#plot_ETH('U2', myData_layers)
+#plot_ETH('E22', myData_layers)
+#plot_ETH('E11', myData_layers)
+#plot_ETH('E33', myData_layers)
+#plot_ETH('E23', myData_layers)
+
+
+# Define fields to plot
+plot_EMax(['E22', 'E11', 'E33', 'E23'], myData_layers) 
